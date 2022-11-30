5529d6dbeadd3f22a0793434f7c9f3326956ac00
mdjawadulhasan <jhjawad111@gmail.com>
2022-11-30 22:33:28 +0600

    Blog Done

--- src/home.js
+++ src/home.js
@@ -1,9 +1,8 @@
-
 import BlogList from "./BlogList";
 import useFetch from "./useFetch";
 
 const Home = () => {
-  const { error, data: blogs,isPending } = useFetch('http://localhost:8000/blogs')
+  const { error, isPending, data: blogs } = useFetch('http://localhost:8000/blogs')
 
   return (
     <div className="home">
